4fedbc144cc5c1599b48610ed6375e333f51fc0c
Paulo Lopes <pmlopes@gmail.com>
2014-07-02 14:34:51 +0200

    fix rabbitmq crash

--- src/test/groovy/com/jetdrone/vertx/mods/stomp/Generator.java
+++ src/test/groovy/com/jetdrone/vertx/mods/stomp/Generator.java
@@ -1,45 +1,34 @@
 package com.jetdrone.vertx.mods.stomp;
 
-import org.apache.activemq.ActiveMQConnection;
-import org.apache.activemq.ActiveMQConnectionFactory;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
-import javax.jms.*;
 import java.io.IOException;
 
 public class Generator {
     private static final String QUEUE_NAME = "tims_test_queue";
 
-    public static void main(String[] args) throws IOException, JMSException {
+    public static void main(String[] args) throws IOException {
 
         int nMessages = args.length > 0 ? Integer.parseInt(args[0]) : 1;
 
-        // Getting JMS connection from the server and starting it
-        ConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_BROKER_URL);
-        Connection connection = factory.createConnection();
-        connection.start();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
-        // JMS messages are sent and received using a Session. We will
-        // create here a non-transactional session object. If you want
-        // to use transactions you should set the first parameter to 'true'
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-        // Destination represents here our queue 'TESTQUEUE' on the
-        // JMS server. You don't have to do anything special on the
-        // server to create it, it will be created automatically.
-        Destination destination = session.createQueue(QUEUE_NAME);
-
-        // MessageProducer is used for sending messages (as opposed
-        // to MessageConsumer which is used for receiving them)
-        MessageProducer producer = session.createProducer(destination);
+        // Create a new durable channel (if one doesn't already exist)
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
 
         // Send nMessages messages
         for (int msg = 1; msg <= nMessages; msg++) {
-            TextMessage message = session.createTextMessage("Hello World! " + msg);
-            // Here we are sending the message!
-            producer.send(message);
+            String message = "Hello World! " + msg;
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
         }
 
+        channel.close();
         connection.close();
     }
 }
